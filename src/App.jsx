d163b7d4819a69979e71ff766b8b979dd1b948c0
Aditya Kumar <adii.utsav@gmail.com>
2026-07-01 15:08:52 +0530

    WIP: save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-
+// import { Button } from "./components/ui/Button";
+import { Button } from "./components/ui/Button";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="font-bold">hello</div>
+      <Button>Book Appointment</Button>
     </>
   );
 }
